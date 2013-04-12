3c25ab3b1b1a9e7fdbe21ebfc78019be0d4bae64
Blaed <blaedj@gmail.com>
2013-04-12 00:05:42 -0500

    project structure cleanup, initially finish the mhcs class

--- src/mhcs/blaed/MHCS.java
+++ src/mhcs/blaed/MHCS.java
@@ -3,14 +3,12 @@ package mhcs.blaed;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabBar;
 import com.google.gwt.user.client.ui.TabPanel;
-import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -42,49 +40,44 @@ public class MHCS implements EntryPoint {
 	enabler.addClickHandler( new ClickHandler(){
 		@Override
 		public void onClick(ClickEvent event) {
-			// TODO Auto-generated method stub
-			enableAllTheTabs();
+		    enableAllTheTabs();
 		}
-		
-	});
-	
+	    });
+
 	RootLayoutPanel.get().add(baseTabPanel);
 	RootLayoutPanel.get().add(enabler);
 	enabler.setVisible(true);
 	baseTabPanel.setVisible(true);
 	baseTabPanel.addStyleName("baseTabber");
     }
-    
+
     protected void enableAllTheTabs() {
     	for ( int i=1;i<baseTabPanel.getTabBar().getTabCount(); i++ ) {
     	    baseTabBar.setTabEnabled(i, true);
-    	}	
-	}
+    	}
+    }
+
     /*
      * @param tabIndex int the index of the tab to insert contents into
-     * @param content Widget the widget (content) to insert into the tab
+     * @param content safeHtml the html widget (content) to insert into the tab
      * @param tabTitle string the new title of the tab
      * @post the tab at the index specified is removed and a new tab is added with contents provided
      */
-    //public void changeTabContents( int tabIndex, Widget contentWidget, String tabTitle ){
-
-    //	baseTabPanel.remove(baseTabPanel.getWidget(tabIndex));
-    //	baseTabPanel.add(contentWidget, tabTitle);
-    //    }
+    public void changeTabContents( int tabIndex, SafeHtml contentHTML, String tabTitle ){
+    	baseTabBar.setTabHTML(tabIndex, contentHTML);
+    	baseTabBar.setTabText(tabIndex, tabTitle);
+    	baseTabBar.setTabEnabled(tabIndex, true);
+    }
 
     /* switches the state of the tab, if enable then disable and vice versa
      * @param tabIndex int the index of the tab to toggle
      * @post the disabled/enabled state of the tab is switched
      */
-    /*public void toggleTab(int tabIndex){
-    //Tab tab = baseTabBar.getTab(tabIndex);
-    if(baseTabBar.isTabEnabled(tabIndex)){
-    baseTabBar.setTabEnabled(tabIndex, false);
-    }
-    else {
-    baseTabBar.setTabEnabled(tabIndex, true);
-    }
-    }*/
-
-    // TODO add a method that keeps the order of the tabs correct?
-} // end MHCS
+    public void toggleTab(int tabIndex){
+	if(baseTabBar.isTabEnabled(tabIndex)){
+	    baseTabBar.setTabEnabled(tabIndex, false);
+	}
+	else {
+	    baseTabBar.setTabEnabled(tabIndex, true);
+	}
+    }// end MHCS
