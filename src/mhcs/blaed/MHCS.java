3ff1cb54fd2314ad1b079d9fa177a4158713be09
Blaed <blaedj@gmail.com>
2013-04-12 15:36:11 -0500

    working with ignore files

--- src/mhcs/blaed/MHCS.java
+++ src/mhcs/blaed/MHCS.java
@@ -16,68 +16,69 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class MHCS implements EntryPoint {
 
-    private TabPanel baseTabPanel;
-    private TabBar baseTabBar;
+	private TabPanel baseTabPanel;
+	private TabBar baseTabBar;
 
-    @Override
+	@Override
 
-    public void onModuleLoad() {
+	public void onModuleLoad() {
 
-	baseTabPanel = new TabPanel();
-	baseTabBar     = baseTabPanel.getTabBar();
+		baseTabPanel = new TabPanel();
+		baseTabBar     = baseTabPanel.getTabBar();
 
-	baseTabPanel.add(new HTML("<div><h1>This is the first tab, Module Logging</h1><p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p><img src='http://lorempixel.com/output/city-q-g-640-480-5.jpg' /></div>"), "Module Logging");
-	baseTabPanel.add(new HTML("<div><img src='http://lorempixel.com/output/nature-q-g-640-480-5.jpg' /></div>"), "Not Logging Page");
+		baseTabPanel.add(new HTML("<div><h1>This is the first tab, Module Logging</h1><p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p><img src='http://lorempixel.com/output/city-q-g-640-480-5.jpg' /></div>"), "Module Logging");
+		baseTabPanel.add(new HTML("<div><img src='http://lorempixel.com/output/nature-q-g-640-480-5.jpg' /></div>"), "Not Logging Page");
 
-	baseTabPanel.selectTab(0);
+		baseTabPanel.selectTab(0);
 
-	for ( int i=1;i<baseTabPanel.getTabBar().getTabCount(); i++ ) {
-	    baseTabBar.setTabEnabled(i, false);
-	}
-
-	Button enabler = new Button();
-	enabler.setText("enable all tabs");
-	enabler.addClickHandler( new ClickHandler(){
-		@Override
-		public void onClick(ClickEvent event) {
-		    enableAllTheTabs();
+		for ( int i=1;i<baseTabPanel.getTabBar().getTabCount(); i++ ) {
+			baseTabBar.setTabEnabled(i, false);
 		}
-	    });
 
-	RootLayoutPanel.get().add(baseTabPanel);
-	RootLayoutPanel.get().add(enabler);
-	enabler.setVisible(true);
-	baseTabPanel.setVisible(true);
-	baseTabPanel.addStyleName("baseTabber");
-    }
+		Button enabler = new Button();
+		enabler.setText("enable all tabs");
+		enabler.addClickHandler( new ClickHandler(){
+			@Override
+			public void onClick(ClickEvent event) {
+				enableAllTheTabs();
+			}
+		});
 
-    protected void enableAllTheTabs() {
-    	for ( int i=1;i<baseTabPanel.getTabBar().getTabCount(); i++ ) {
-    	    baseTabBar.setTabEnabled(i, true);
-    	}
-    }
-
-    /*
-     * @param tabIndex int the index of the tab to insert contents into
-     * @param content safeHtml the html widget (content) to insert into the tab
-     * @param tabTitle string the new title of the tab
-     * @post the tab at the index specified is removed and a new tab is added with contents provided
-     */
-    public void changeTabContents( int tabIndex, SafeHtml contentHTML, String tabTitle ){
-    	baseTabBar.setTabHTML(tabIndex, contentHTML);
-    	baseTabBar.setTabText(tabIndex, tabTitle);
-    	baseTabBar.setTabEnabled(tabIndex, true);
-    }
-
-    /* switches the state of the tab, if enable then disable and vice versa
-     * @param tabIndex int the index of the tab to toggle
-     * @post the disabled/enabled state of the tab is switched
-     */
-    public void toggleTab(int tabIndex){
-	if(baseTabBar.isTabEnabled(tabIndex)){
-	    baseTabBar.setTabEnabled(tabIndex, false);
+		RootLayoutPanel.get().add(baseTabPanel);
+		RootLayoutPanel.get().add(enabler);
+		enabler.setVisible(true);
+		baseTabPanel.setVisible(true);
+		baseTabPanel.addStyleName("baseTabber");
 	}
-	else {
-	    baseTabBar.setTabEnabled(tabIndex, true);
+
+	protected void enableAllTheTabs() {
+		for ( int i=1;i<baseTabPanel.getTabBar().getTabCount(); i++ ) {
+			baseTabBar.setTabEnabled(i, true);
+		}
 	}
-    }// end MHCS
+
+	/*
+	 * @param tabIndex int the index of the tab to insert contents into
+	 * @param content safeHtml the html widget (content) to insert into the tab
+	 * @param tabTitle string the new title of the tab
+	 * @post the tab at the index specified is removed and a new tab is added with contents provided
+	 */
+	public void changeTabContents( int tabIndex, SafeHtml contentHTML, String tabTitle ){
+		baseTabBar.setTabHTML(tabIndex, contentHTML);
+		baseTabBar.setTabText(tabIndex, tabTitle);
+		baseTabBar.setTabEnabled(tabIndex, true);
+	}
+
+	/* switches the state of the tab, if enable then disable and vice versa
+	 * @param tabIndex int the index of the tab to toggle
+	 * @post the disabled/enabled state of the tab is switched
+	 */
+	public void toggleTab(int tabIndex){
+		if(baseTabBar.isTabEnabled(tabIndex)){
+			baseTabBar.setTabEnabled(tabIndex, false);
+		}
+		else {
+			baseTabBar.setTabEnabled(tabIndex, true);
+		}
+	}
+} // end MHCS
